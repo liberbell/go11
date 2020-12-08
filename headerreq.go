09fa450778f28a5f440c9e2139285b8ffdbe0bac
Liberbell <liberbell@gmail.com>
2020-12-09 08:54:24 +0900

    headerreq3

--- headerreq.go
+++ headerreq.go
@@ -1,6 +1,10 @@
 package main
 
-import "net/http"
+import (
+	"fmt"
+	"net/http"
+	"net/http/httputil"
+)
 
 func main() {
 	url1 := "https://www.yahoo.co.jp"
@@ -10,4 +14,7 @@ func main() {
 
 	client := net(http.Client)
 	resp, err := client.Do(req)
+
+	dumpResp, _ := httputil.DumpResponse(resp, true)
+	fmt.Printf("%s", dumpResp)
 }
