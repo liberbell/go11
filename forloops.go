ac4d9ffad69ce670afef15f275e2de6e9288385a
Liberbell <liberbell@gmail.com>
2020-12-11 09:13:22 +0900

    forloops5

--- forloops.go
+++ forloops.go
@@ -3,9 +3,12 @@ package main
 import "fmt"
 
 func main() {
-	z := 1
-	for z <= 5 {
+	// z := 1
+	// for z <= 5 {
+	// 	fmt.Println(z)
+	// 	z = z + 1
+	// }
+	for z := 1; z <= 5; z++ {
 		fmt.Println(z)
-		z = z + 1
 	}
 }
