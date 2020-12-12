b004d0a8f486fa71fec084e018b6f6f507333405
Liberbell <liberbell@gmail.com>
2020-12-12 21:36:03 +0900

    switchstatement3

--- switchstatement.go
+++ switchstatement.go
@@ -1,5 +1,16 @@
 package main
 
-func main() {
+import "fmt"
 
+func main() {
+	x := 4
+	fmt.Println("This is ", x)
+	switch x {
+	case 3:
+		fmt.Println("Three")
+	case 4:
+		fmt.Println("Four")
+	case 5:
+		fmt.Println("Five")
+	}
 }
