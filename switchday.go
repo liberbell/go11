2937dbb3e38461d2f1ac19a5063616b28884b634
Liberbell <liberbell@gmail.com>
2020-12-12 21:46:09 +0900

    switchday3

--- switchday.go
+++ switchday.go
@@ -7,7 +7,7 @@ import (
 
 func main() {
 	switch time.Now().Weekday() {
-	case time.Monday, time.Thursday, time.Wednesday, time.Thursday, time.Friday:
+	case time.Monday, time.Tuesday, time.Wednesday, time.Thursday, time.Friday:
 		fmt.Println("It's a weekday")
 	default:
 		fmt.Println("It's the weekend")
